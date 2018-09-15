893c92d24f663fd6295d6886e760041d2d6ff9fa
aduarte <alexa.duarte@ucr.ac.cr>
2018-09-14 18:02:57 -0600

    Documentación (comentarios)

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <stdbool.h>
 
 void open_directory();
-bool read_file(char* file_name);
+char* read_file(char* file_name);
 int find_password(char* word);
 void error(const char *s);
 
@@ -18,55 +18,52 @@ int main() {
     return 0;
 }
 
-//https://poesiabinaria.net/2011/09/listar-archivos-dentro-de-un-directorio-o-carpeta-en-c/
+//Tomado de https://poesiabinaria.net/2011/09/listar-archivos-dentro-de-un-directorio-o-carpeta-en-c/
 void open_directory() {
     DIR *dir;
     struct dirent *ent;
 
-    /*char* path;
-
-    Directorio actual */
-    /* char cwd[PATH_MAX];
-     if (getcwd(cwd, sizeof(cwd)) != NULL) {
-         char* name_dir = "/lists";
-         path = malloc(strlen(cwd)+1+6);
-         strcpy(path, cwd);
-         strcat(path, name_dir);
-
-         //printf("Current working dir: %s\n", path);
-     } else {
-         perror("getcwd() error");
-         exit;
-     }*/
-
-    dir = opendir ("/home/aduarte/CLionProjects/PasswordDecoder/lists"); //Abre directorio
+    dir = opendir ("/home/aduarte/CLionProjects/PasswordDecoder/lists"); //Abre directorio TODO Hacerlo para cualquier dir
 
     if (dir == NULL)
         error("lists.");
 
-    bool found = false;
+    bool found = false; //Condicion de parada
+    char* password; //Donde se guarda la palabra encontrada
 
     while (((ent = readdir (dir)) != NULL )&& !found)
     {
         /* Devolverá directorio actual (.) y el anterior (..)*/
         if ( (strcmp(ent->d_name, ".")!=0) && (strcmp(ent->d_name, "..")!=0) )
         {
-            found = read_file(ent->d_name); //Llama a leer archivo
+            password = read_file(ent->d_name); //Llama a leer archivo actual
+            if(strcmp(password,"-1") != 0) { //Si la encuentra se detiene
+                found = true;
+            }
         }
+    }
+
+    if(found) {
+        printf("Password found: %s\n", password);
+    } else {
+        printf("Sorry, password not found.\n");
     }
 
     closedir (dir);
 }
 
-//https://gsamaras.wordpress.com/code/read-file-line-by-line-in-c-and-c/
-bool read_file(char* file_name) {
+//Tomado de https://gsamaras.wordpress.com/code/read-file-line-by-line-in-c-and-c/
+//-1 es que no encontró el password. Caso conrario se devuelve la palabra que hizo match.
+char* read_file(char* file_name) {
     //Construye el path
     char* name_dir = "/home/aduarte/CLionProjects/PasswordDecoder/lists/";
-    char* path = malloc(strlen(name_dir)+1+11);
-    char* password = malloc(sizeof(char)*4);
+    char* path = malloc(strlen(name_dir)+100);
     strcpy(path, name_dir);
     strcat(path, file_name);
-    bool found = false;
+
+    bool found = false; //Condición de parada
+    char* password = malloc(sizeof(char)*4);
+    strcpy(password, "-1"); //Inicializa el valor
 
     FILE* fp;
     char buf[BUF_SIZE];
@@ -79,24 +76,22 @@ bool read_file(char* file_name) {
     while ((fgets(buf, sizeof(buf), fp) != NULL) && !found) {
         buf[strlen(buf) - 1] = '\0';
 
-        if(find_password(buf) == 1) {
+        if(find_password(buf) == 1) { //Guarda palabra
             strcpy(password, buf);
             found = true;
         }
     }
 
-    if(found) {
-        printf("Password found: %s\n", password);
-    } else {
-        printf("Sorry, password not found.\n");
-    }
-
     fclose(fp);
 
-    return found;
+    free(path);
+
+    return password;
 }
 
+//Compara hash generado con el brindado por el profesor
 int find_password(char* word) {
+    //Construye comando
     int found = 0;
     char *command = "openssl passwd -1 -salt SALt ";
     char* cmd = malloc(strlen(command)+1+50);
@@ -122,6 +117,8 @@ int find_password(char* word) {
         printf("Command not found or exited with error status\n");
         found = -1;
     }
+
+    free(cmd);
 
     return found;
 }
